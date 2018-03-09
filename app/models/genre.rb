ee394e57eaf474f8eaac17c98e3f9094bc7f3f43
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-09 03:24:07 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,17 +3,17 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    genre.songs.length
+    self.songs.length
     # return the number of songs in a genre
   end
 
   def artist_count
-    genre.artist.length
+    self.artist.length
     # return the number of artists associated with the genre
   end
 
   def all_artist_names
-    genre.artists map do |artist|
+    self.artists map do |artist|
       artist.name
     end
     # return an array of strings containing every musician's name
